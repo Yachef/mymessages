24d062d734739e60aec02cb36d24f247e500d93f
Yachef <yachef.h@gmail.com>
2020-08-18 05:33:13 +0200

    prod ready

---
+++ backend/routes/posts.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const PostsController = require('../controllers/posts')
+
+const router = express.Router();
+
+const checkAuth = require('../middleware/check-auth')
+const extractFile = require('../middleware/file')
+
+router.post("",checkAuth,extractFile, PostsController.createPost)
+
+router.get("",PostsController.getPosts)
+
+router.delete("/:id", checkAuth, PostsController.deletePost)
+
+router.put("/:id", checkAuth,extractFile, PostsController.updatePost)
+
+router.get("/:id",PostsController.getPost)
+
+module.exports = router;
